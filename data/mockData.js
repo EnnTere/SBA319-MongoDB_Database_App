da7fcf4ece2f72dd9c46849a53614299bc7dccba
Natalie <52940911+NatUXR@users.noreply.github.com>
2025-02-02 18:37:09 -0700

    Added server, expanded data organization, added temp mock data.

---
+++ data/mockData.js
@@ -0,0 +1,24 @@
+// Mock database
+
+const mockData = [
+  {
+    id: "1", 
+    name: "catPic",
+    pet: "cat",
+  },
+  {
+    id: "2", 
+    name: "lizardNoPic",
+    pet: "lizard",
+  },
+  {
+    id: "3", 
+    name: "frogPic",
+    pet: "frog",
+  },
+];
+
+
+
+
+module.exports = mockData;
---
+++ index.js
@@ -0,0 +1,62 @@
+// Server
+const express = require("express");
+const app = express();
+const port = 7000;
+
+const { error } = require("console");
+
+// app.set ("", "")
+// app.set ("", "")
+
+// Root Route
+app.get("/", (req, res) => {
+  res.json("You are at root");
+});
+
+
+// Routes
+// Import Routes
+
+// Routes
+
+//////////////////////
+///// Middleware /////
+//////////////////////
+
+///// Body Parsers /////
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+
+
+///// Request Logging /////
+app.use((req, res, next) => {
+  const logTime = new Date();
+  console.log(`
+    -------
+    ${logTime.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}
+    `);
+  if (Object.keys(req.body).length > 0) {
+    console.log(`Data contained: ${JSON.stringify(req.body)}`);
+    // console.log();  
+  }
+});
+
+
+
+///// Errors /////
+
+// 404 not found
+app.use((req, res, next) => {
+  next(error(404, "Resoure Not Found"))
+});
+
+// General Error Handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err.message })
+});
+
+// Port
+app.listen(port, () => {
+  console.log(`Server listening to ${port}`);
+});
